b727827aa4b11572dce69e57577eae88d19fe349
akshayverma21 <120587872+akshayverma21@users.noreply.github.com>
2025-07-06 19:34:58 +0530

    Update settings.py
    update setting added whitenoise

--- audio_converter/settings.py
+++ audio_converter/settings.py
@@ -70,13 +70,14 @@ NPM_BIN_PATH = 'npm.cmd'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'audio_converter.urls'
@@ -183,6 +184,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATICFILES_DIRS= [os.path.join(BASE_DIR, 'audio_converter', 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL= '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
